94e7a26fd4c38792fad3956f09b4f4e45e29e1d7
Andrii Khomych <andriy.khomych@gmail.com>
2019-06-26 19:25:44 +0300

    Fix travis.

--- .travis-phpcs.sh
+++ .travis-phpcs.sh
@@ -7,6 +7,6 @@ set -e $DRUPAL_TI_DEBUG
 drupal_ti_ensure_drupal
 
 echo "codesniffer standard Drupal"
-phpcs --report=full --warning-severity=0 --standard=Drupal "$DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME" || false
+$DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpcs --report=full --warning-severity=0 --standard=Drupal "$DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME" || false
 echo "codesniffer standard DrupalPractice"
-phpcs --report=full --standard=DrupalPractice "$DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME" || false
+$DRUPAL_TI_DRUPAL_DIR/vendor/bin/phpcs --report=full --standard=DrupalPractice "$DRUPAL_TI_DRUPAL_DIR/modules/$DRUPAL_TI_MODULE_NAME" || false
